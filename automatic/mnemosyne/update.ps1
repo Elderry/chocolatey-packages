b216cda722d25f8f4bd32a74aa31e669c2aecc29
Ruiyang Li <Elderry@outlook.com>
2017-01-08 13:56:26 +0800

    Fixed match regex for mnemosyne version

--- automatic/mnemosyne/update.ps1
+++ automatic/mnemosyne/update.ps1
@@ -1,10 +1,10 @@
 function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri 'http://mnemosyne-proj.org/download-mnemosyne.php'
-    $download_page.Content -Match '\d+\.\d+\.\d+' | Out-Null
-    $version = $Matches[0]
     $url = $download_page.Links |
            Where-Object innerHTML -Match 'Windows' |
            Select-Object -First 1 -ExpandProperty href
+    $url -match '[/]mnemosyne-([\d|.]*)[/]' | Out-Null
+    $version = $Matches[1]
     return @{
         Version = $version
         URL32 = $url
