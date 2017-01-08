a3873aff38e0776eafc625b237d7c180c1e0d97f
Chocolatey <chocolatey@realdimensions.net>
2017-01-08 06:08:23 +0000

    AU: 1 updated - mnemosyne [skip ci] https://gist.github.com/687b1756f34c3b9f913dfb64ed3e2ca4/a777a581cc2cda521dc49f7439f9292c68525630

--- automatic/mnemosyne/tools/chocolateyinstall.ps1
+++ automatic/mnemosyne/tools/chocolateyinstall.ps1
@@ -1,11 +1,11 @@
-﻿$ErrorActionPreference = 'Stop'
+$ErrorActionPreference = 'Stop'
 
 $packageArgs = @{
     packageName    = 'mnemosyne'
     fileType       = 'EXE'
-    url            = 'http://sourceforge.net/projects/mnemosyne-proj/files/mnemosyne/mnemosyne-2.3.6/mnemosyne-2.3.6-setup.exe/download'
+    url            = 'http://sourceforge.net/projects/mnemosyne-proj/files/mnemosyne/mnemosyne-2.4/mnemosyne-2.4-setup.exe/download'
     softwareName   = 'Mnemosyne'
-    checksum       = 'c099c443af68953c79d0ee061d99197d'
+    checksum       = '5dc38bef26ff0152da27508c1ae19352'
     checksumType   = 'MD5'
     silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
     validExitCodes = @(0)
