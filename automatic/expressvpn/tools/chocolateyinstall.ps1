2e5611c983c51f2339f88c76083c6f8bbf9dd31d
Chocolatey <chocolatey@realdimensions.net>
2017-02-07 22:04:19 +0000

    AU: 1 updated - expressvpn [skip ci] https://gist.github.com/687b1756f34c3b9f913dfb64ed3e2ca4/a97a21b6b6d421b304585c35ba62d9ff36d34f3a

--- automatic/expressvpn/tools/chocolateyinstall.ps1
+++ automatic/expressvpn/tools/chocolateyinstall.ps1
@@ -3,9 +3,9 @@
 $packageArgs = @{
     packageName    = 'expressvpn'
     fileType       = 'EXE'
-    url            = 'https://download.express-vpn.xyz/clients/windows/expressvpn_6.0.7.1305.exe'
+    url            = 'https://download.express-vpn.xyz/clients/windows/expressvpn_6.0.8.1371.exe'
     softwareName   = 'Express VPN'
-    checksum       = '283968ed84929bf7344746f004bbca6a'
+    checksum       = '52a31c9c6314dd05c0f6bd6e81cedbf9'
     checksumType   = 'MD5'
     silentArgs     = '/S'
     validExitCodes = @(0)
