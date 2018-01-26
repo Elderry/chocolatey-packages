c7bf864c056c66d6247095a7de7da00558014771
Ruiyang <Elderry@outlook.com>
2018-01-26 14:47:22 +0800

    Fixed mnemosyne's update script.

--- automatic/mnemosyne/update.ps1
+++ automatic/mnemosyne/update.ps1
@@ -1,8 +1,9 @@
 ﻿function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri 'http://mnemosyne-proj.org/download-mnemosyne.php'
-    $url = $download_page.Links |
-           Where-Object innerHTML -Match 'Windows' |
-           Select-Object -First 1 -ExpandProperty href
+    $url = $download_page.Links.outerHTML |
+           Where-Object { $_ -Match 'Windows' } |
+           Select-Object -First 1
+    $url = $url.split('"')[1]
     $url -match '[/]mnemosyne-([\d|.]*)[/]' | Out-Null
     $version = $Matches[1]
     return @{
