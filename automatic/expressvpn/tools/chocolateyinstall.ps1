7924fc7b96c31e6dc7e3d044aa4f9ed25148e139
Chocolatey <chocolatey@realdimensions.net>
2018-07-03 02:27:09 +0000

    AU: 2 updated - expressvpn sharemouse [skip ci] https://gist.github.com/687b1756f34c3b9f913dfb64ed3e2ca4/20ca81e7b00e357a816b9413227680fdd7c630c4

--- automatic/expressvpn/tools/chocolateyinstall.ps1
+++ automatic/expressvpn/tools/chocolateyinstall.ps1
@@ -3,9 +3,9 @@
 $packageArgs = @{
     packageName    = 'expressvpn'
     fileType       = 'EXE'
-    url            = 'https://download.expressvpn.xyz/clients/windows/expressvpn_6.5.0.3428.exe'
+    url            = 'https://download.expressvpn.xyz/clients/windows/expressvpn_6.6.2.4493.exe'
     softwareName   = 'Express VPN'
-    checksum       = '52058b21a220fd6decf856f9b82b3556'
+    checksum       = '2c62c042f27604f2ad91f797b2cd4614'
     checksumType   = 'MD5'
     silentArgs     = '/S'
     validExitCodes = @(0)
