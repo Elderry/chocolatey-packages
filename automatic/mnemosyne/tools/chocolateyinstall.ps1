395aecd8bc659a1d8eb573cebb48205386d1bf1c
Ruiyang Li <Elderry@outlook.com>
2019-12-14 23:22:04 +0800

    update mnemosyne to 2.7

--- automatic/mnemosyne/tools/chocolateyinstall.ps1
+++ automatic/mnemosyne/tools/chocolateyinstall.ps1
@@ -3,10 +3,8 @@
 $packageArgs = @{
     packageName    = 'mnemosyne'
     fileType       = 'EXE'
-    url            = 'https://sourceforge.net/projects/mnemosyne-proj/files/mnemosyne/mnemosyne-2.6/Mnemosyne-2.6.exe/download'
+    url            = 'https://sourceforge.net/projects/mnemosyne-proj/files/mnemosyne/mnemosyne-2.7/mnemosyne-2.7-setup.exe/download'
     softwareName   = 'Mnemosyne'
-    checksum       = '9830ce352df608f6463ecea4a80871fd'
-    checksumType   = 'MD5'
     silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
     validExitCodes = @(0)
 }
